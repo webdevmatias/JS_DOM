51ee74bff3e628c53e6d2042f5d3d38b222742f4
Lucas Matias <99132374+webdevmatias@users.noreply.github.com>
2024-04-08 13:44:25 -0300

    Add files via upload
    comandos básicos para a manipulação do dom

---
+++ index.js
@@ -0,0 +1,50 @@
+
+
+/*
+ -----> EXEMPLO INTRODUTORIO <-------
+
+
+const text = document.querySelector('h1');
+text.innerHTML = "olá mundo";
+text.style.color = "#f33"
+
+ -----> FIM EX. INTRODUTORIO <-------
+*/
+
+/*
+ -----> <-------
+ALERT()
+PROMPT()
+CONFIRM()
+ ----->  <-------
+*/
+
+/*
+ -----> SELETORES <-------
+
+const texto = document.getElementById('id')
+const texto = document.getElementByClassName('classe')
+const texto = document.getElementByName('name')
+const texto = document.getElementsTagName('tagmane')
+
+
+querySelector()
+querySelectorAll()
+ ----->  <-------
+*/
+
+/*
+ -----> innerHTML <-------
+
+capturando conteudo contido no elemento
+
+const texto = document.getElementById('text')
+const content = 'ola mundo!'
+texto.innerHTML = content
+console.log(content)
+
+ ----->  <-------
+*/
+
+
+
